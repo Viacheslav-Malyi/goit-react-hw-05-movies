4baa21d8c48f30edc4960e7dd9c685cd8e844482
Viacheslav-Malyi <Malyislava@gmail.com>
2022-11-15 17:43:48 +0100

    1

--- src/components/Layayt/Layout.jsx
+++ src/components/Layayt/Layout.jsx
@@ -1,5 +1,5 @@
 import { Suspense } from 'react';
-import { NavLink, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { Container, Header, Link } from './Layout.styled';
 export const Layout = () => {
   return (
